53cce75993f5747c42888944351af189c99fe5f9
Virghileanu Teodor <teo.virghi@yahoo.ro>
2022-08-20 21:27:01 +0300

    add other preferred constants
    TODO use those constants when adequate

--- ai/preferences.py
+++ ai/preferences.py
@@ -8,6 +8,11 @@ from numpy import ndarray
 SAMPLE_RATE = 44100
 CONVERT_TO_MONO = True
 MAX_DURATION = 1.5
+MIN_DURATION = 0.01
+SILENCE_DB = 60
+
+FADE_IN_DURATION = 0.005
+FADE_OUT_DURATION = 0.01
 
 N_MFCC = 20
 
@@ -27,7 +32,7 @@ def load_sound(file_path: str) -> tuple[ndarray, int, float]:
     )
     (trimmed_silence, _) = trim(
         y=librosa.util.normalize(samples),
-        top_db=60,
+        top_db=SILENCE_DB,
     )
 
     return trimmed_silence, sample_rate, librosa.get_duration(y=trimmed_silence, sr=sample_rate)
